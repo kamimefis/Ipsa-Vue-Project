7881788fd1492c21010b6fe0240b81d1f7a55e66
Camila <camilalobos.2307@gmail.com>
2024-12-10 14:15:12 -0300

    app render view

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
 <script setup>
 import ChartComponent from './components/ChartComponent.vue';
 import DatatableComponent from './components/DatatableComponent.vue'
+import HeaderComponent from './components/HeaderComponent.vue';
 import SearchBarComponent from './components/SearchBarComponent.vue';
+import SummaryComponent from './components/SummaryComponent.vue';
 
 
 </script>
 
 <template>
-  <SearchBarComponent/>
-  <ChartComponent />
+  <SearchBarComponent />
+  <HeaderComponent />
+  <div class="container">
+    <div class="chart">
+      <ChartComponent />
+    </div>
+    <div class="summary">
+      <SummaryComponent />
+    </div>
+  </div>
+
   <DatatableComponent />
 </template>
 
-<style></style>
+<style>
+.container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin-top: 10px;
+}
+
+.chart {
+    width: 75%;
+}
+.summary {
+    width: 25%;
+}
+
+
+@media (max-width: 768px) {
+    .container {
+        flex-direction: column;
+    }
+    .chart,
+    .summary {
+        width: 100%; 
+        margin-bottom: 10px;
+    }
+}
+</style>
